8cc7af5ffc77c254926c3980c659b8d88f963ccb
Shane DiDona <shanedidona@gmail.com>
2026-05-26 21:48:28 -0400

    [BENDER-1] public sealed class Or2D : Region2D

---
+++ bender/Bender.Lib.NET/Objects/Region2D/Or2D.cs
@@ -0,0 +1,30 @@
+﻿namespace Bender.Lib.NET
+{
+    public sealed class Or2D : Region2D
+    {
+        readonly Region2D[] _regions;
+
+        public Or2D(Region2D[] regions)
+        {
+            if (regions.Length == 0)
+            {
+                throw new NotSupportedException("!regions.Any() is not supported for now.");
+            }
+
+            _regions = regions;
+        }
+
+        public override bool IsIn(double x, double y, double epsilon)
+        {
+            foreach (Region2D region in _regions)
+            {
+                if (region.IsIn(x, y, epsilon))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
