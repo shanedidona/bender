074eee40412a0ed6215062f2cce541a2eead1335
Shane DiDona <shanedidona@gmail.com>
2026-05-28 18:37:14 -0400

    [BENDER-1] public sealed class AButNotB2D : Region2D

---
+++ bender/Bender.Lib.NET/Objects/Region2D/AButNotB2D.cs
@@ -0,0 +1,19 @@
+﻿namespace Bender.Lib.NET
+{
+    public sealed class AButNotB2D : Region2D
+    {
+        readonly Region2D _a;
+        readonly Region2D _b;
+
+        public AButNotB2D(Region2D a, Region2D b)
+        {
+            _a = a;
+            _b = b;
+        }
+
+        public override bool IsIn(double x, double y, double epsilon)
+        {
+            return _a.IsIn(x, y, epsilon) && (!_b.IsIn(x, y, epsilon));
+        }
+    }
+}
