f06f26e34852157d83c3a1334ee6b7ec2a94d098
Shane DiDona <shanedidona@gmail.com>
2026-05-28 19:38:06 -0400

    [BENDER-1] copy over logger code from personal project

--- bender/benderEXE/Program.cs
+++ bender/benderEXE/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace benderEXE
+﻿using Serilog;
+
+namespace benderEXE
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string template = "[{Timestamp:HH:mm:ss.fff} {Level:u3}][{SourceContext}] {Message:lj}{NewLine}{Exception}";
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console(outputTemplate: template)
+                //.WriteTo.File(outputTemplate: template, path: "", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            Log.Information("Logger made");
+
+
+
+
+
+
+
+
+
         }
     }
 }
