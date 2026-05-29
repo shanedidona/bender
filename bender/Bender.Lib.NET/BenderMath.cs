d51b55e89e456ee1a0cdaa7752ea65627fc74899
Shane DiDona <shanedidona@gmail.com>
2026-05-28 20:00:54 -0400

    [BENDER-1] public static Mat RenderMat(ElectrostaticGrid2D electrostaticGrid2D)

--- bender/Bender.Lib.NET/BenderMath.cs
+++ bender/Bender.Lib.NET/BenderMath.cs
@@ -1,10 +1,33 @@
-﻿namespace Bender.Lib.NET
+﻿using OpenCvSharp;
+
+namespace Bender.Lib.NET
 {
     public static class BenderMath
     {
         public static double Sq(double x)
         {
             return x * x;
+        }
+
+        public static Mat RenderMat(ElectrostaticGrid2D electrostaticGrid2D)
+        {
+            var vec3BArray = new Vec3b[electrostaticGrid2D.NY, electrostaticGrid2D.NX];
+            for (int i = 0; i < electrostaticGrid2D.NY; i++)
+            {
+                for (int j = 0; j < electrostaticGrid2D.NX; j++)
+                {
+                    if (electrostaticGrid2D.ID[j, electrostaticGrid2D.NY - i - 1] == 0)
+                    {
+                        vec3BArray[i, j] = new Vec3b(255, 255, 255);
+                    }
+                    else
+                    {
+                        vec3BArray[i, j] = new Vec3b(0, 0, 0);
+                    }
+                }
+            }
+
+            return Mat.FromPixelData(electrostaticGrid2D.NY, electrostaticGrid2D.NX, MatType.CV_8UC3, vec3BArray);
         }
     }
 }
