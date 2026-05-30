cb6eda6218cfe6df1c1b3c287716a2cb8b8037a2
Shane DiDona <shanedidona@gmail.com>
2026-05-29 22:18:08 -0400

    [BENDER-5] change to public abstract bool IsIn(double x, double y, double dilation);

--- bender/Bender.Lib.NET/Objects/Region2D/Region2D.cs
+++ bender/Bender.Lib.NET/Objects/Region2D/Region2D.cs
@@ -2,7 +2,7 @@
 {
     public abstract class Region2D
     {
-        public abstract bool IsIn(double x, double y, double epsilon);//Errs on the side of being in vs out.
+        public abstract bool IsIn(double x, double y, double dilation);
 
         public abstract Rectangle LooseBoundingRectangle { get; }//Can be bigger than true bounding rectangle but not smaller in any way; used to accelerate IsIn.
     }
