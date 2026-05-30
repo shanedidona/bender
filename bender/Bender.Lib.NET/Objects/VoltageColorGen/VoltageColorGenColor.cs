d4e6ac0a2832a0349f1252687fb9b3e5ccc72ec4
Shane DiDona <shanedidona@gmail.com>
2026-05-30 16:04:22 -0400

    [BENDER-7] public sealed class VoltageColorGenColor : IVoltageColorGen and use

---
+++ bender/Bender.Lib.NET/Objects/VoltageColorGen/VoltageColorGenColor.cs
@@ -0,0 +1,23 @@
+﻿using OpenCvSharp;
+
+namespace Bender.Lib.NET
+{
+    public sealed class VoltageColorGenColor : IVoltageColorGen
+    {
+        readonly byte _r;
+        readonly byte _g;
+        readonly byte _b;
+
+        public VoltageColorGenColor(byte r, byte g, byte b)
+        {
+            _r = r;
+            _g = g;
+            _b = b;
+        }
+
+        public Vec3b GenColor(double v)
+        {
+            return new Vec3b(_b, _g, _r);
+        }
+    }
+}
