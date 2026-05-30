23f1f47acdf7c34c345d554d8c323f013139f9e2
Shane DiDona <shanedidona@gmail.com>
2026-05-30 15:46:48 -0400

    [BENDER-7] change to      public static Mat RenderMat(ElectrostaticGrid2D electrostaticGrid2D, IVoltageColorGen voltageColorGen)

--- bender/Bender.Lib.NET/BenderMath.cs
+++ bender/Bender.Lib.NET/BenderMath.cs
@@ -9,7 +9,7 @@ namespace Bender.Lib.NET
             return x * x;
         }
 
-        public static Mat RenderMat(ElectrostaticGrid2D electrostaticGrid2D)
+        public static Mat RenderMat(ElectrostaticGrid2D electrostaticGrid2D, IVoltageColorGen voltageColorGen)
         {
             var vec3BArray = new Vec3b[electrostaticGrid2D.NY, electrostaticGrid2D.NX];
             for (int i = 0; i < electrostaticGrid2D.NY; i++)
@@ -18,7 +18,7 @@ namespace Bender.Lib.NET
                 {
                     if (electrostaticGrid2D.ID[j, electrostaticGrid2D.NY - i - 1] == 0)
                     {
-                        vec3BArray[i, j] = new Vec3b(255, 255, 255);
+                        vec3BArray[i, j] = voltageColorGen.GenColor(electrostaticGrid2D.V[i, j]);
                     }
                     else
                     {
