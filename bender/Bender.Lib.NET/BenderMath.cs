5743c8cb2d457cda4f762454b521acd660550396
Shane DiDona <shanedidona@gmail.com>
2026-05-31 14:46:14 -0400

    [BENDER-9] Serilog.Log.Information("meanAbsResid {meanAbsResid}", meanAbsResid);

--- bender/Bender.Lib.NET/BenderMath.cs
+++ bender/Bender.Lib.NET/BenderMath.cs
@@ -93,6 +93,7 @@ namespace Bender.Lib.NET
                 }
 
                 double meanAbsResid = residAbsSum / numResid;
+                Serilog.Log.Information("meanAbsResid {meanAbsResid}", meanAbsResid);
 
                 out1List.Add(meanAbsResid);
                 numResid = 0;
