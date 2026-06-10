e0174cd9e18fe2e547ce4cdb77eb6df1231a2c4b
Shane DiDona <shanedidona@gmail.com>
2026-06-09 20:04:28 -0400

    [BENDER-18] two corners

--- bender/Bender.Lib.NET/BenderMath.cs
+++ bender/Bender.Lib.NET/BenderMath.cs
@@ -261,7 +261,43 @@ namespace Bender.Lib.NET
                 }
                 #endregion
 
+                #region Corners
+                {
+                    #region i==0; j==0
+                    {
+                        int i = 0;
+                        int j = 0;
+                        if (id[i, j] == 0)
+                        {
+                            double neighborMean = 0.5 * (v[i - 1, j] + v[i, j + 1]);
+                            double residual = v[i, j] - neighborMean;
 
+                            residAbsSum += Math.Abs(residual);
+                            numResid++;
+                            v[i, j] -= relaxationFactor * residual;
+                        }
+                    }
+                    #endregion
+
+                    #region i==nx-1; j==0
+                    {
+                        int i = nx-1;
+                        int j = 0;
+                        if (id[i, j] == 0)
+                        {
+                            double neighborMean = 0.5 * (v[i + 1, j] + v[i, j + 1]);
+                            double residual = v[i, j] - neighborMean;
+
+                            residAbsSum += Math.Abs(residual);
+                            numResid++;
+                            v[i, j] -= relaxationFactor * residual;
+                        }
+                    }
+                    #endregion
+
+
+                }
+                #endregion
 
 
 
