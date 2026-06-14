1677ba3ce5b65f821c283ffc537cff392e4fc001
Shane DiDona <shanedidona@gmail.com>
2026-06-14 14:16:30 -0400

    [BENDER-22] skeleton CPP and CS (thx Claude AI)

--- bender/Bender.Lib.NET/InteropClass.cs
+++ bender/Bender.Lib.NET/InteropClass.cs
@@ -2,9 +2,53 @@
 
 namespace Bender.Lib.NET.Interop
 {
-    public static class InteropClass
+    public static unsafe class InteropClass
     {
         [DllImport("Bender.Lib.Native", CallingConvention = CallingConvention.Cdecl)]
         public static extern int Add(int a, int b);
+
+        [DllImport("Bender.Lib.Native", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int Solve1(
+                double* v,
+                ushort* id,
+                int nx,
+                int ny,
+                double relaxationFactor,
+                double meanAbsChangeStop,
+                int maxTries,
+                double** outMeanAbsChangeArray,
+                int* outMeanAbsChangeArrayLen);
+
+        [DllImport("Bender.Lib.Native", CallingConvention = CallingConvention.Cdecl)]
+        static extern void DeleteDoubleArray(double* pointer);
+
+        public static (double[] MeanAbsChangeArray, bool Finished) Solve2DFieldSingleStageCPP(
+                double[,] v,
+                ushort[,] id,
+                double relaxationFactor,
+                double meanAbsChangeStop,
+                int maxTries
+            )
+        {
+            int nx = v.GetLength(0);
+            int ny = v.GetLength(1);
+
+            double* meanAbsChangeArrayPtr = null;
+            int meanAbsChangeArrayLen = 0;
+            int finishedInt;
+
+
+            fixed (double* pv = v)
+            {
+                fixed (ushort* pid = id)
+                {
+                    finishedInt = Solve1(pv, pid, nx, ny, relaxationFactor, meanAbsChangeStop, maxTries, &meanAbsChangeArrayPtr, &meanAbsChangeArrayLen);
+                }
+            }
+
+            var meanAbsChangeArray = new double[meanAbsChangeArrayLen];
+            Marshal.Copy((IntPtr)meanAbsChangeArrayPtr, meanAbsChangeArray, 0, meanAbsChangeArrayLen);
+            return (meanAbsChangeArray, finishedInt != 0);
+        }
     }
 }
--- bender/Bender.Lib.Native/bender1.cpp
+++ bender/Bender.Lib.Native/bender1.cpp
@@ -1,3 +1,5 @@
+#include <cstdint>
+#include <vector>
 #ifdef _WIN32
 	#define EXPORT extern "C" __declspec(dllexport)
 #else
@@ -9,6 +11,40 @@ EXPORT int Add(int a, int b)
 	return a + b;
 }
 
+EXPORT int Solve1(
+	double* v,
+	const uint16_t* id,
+	int nx,
+	int ny,
+	double relaxationFactor,
+	double meanAbsChangeStop,
+	int maxTries,
+	double** outMeanAbsChangeArray,
+	int* outMeanAbsChangeArrayLen
+)
+{
+	std::vector<double> woooo;
+	woooo.push_back(2.3);
 
+
+
+
+
+
+
+
+	int outArrayLen = woooo.size();
+	double* outArray = new double[outArrayLen];
+	std::copy(woooo.begin(), woooo.end(), outArray);
+	*outMeanAbsChangeArray = outArray;
+	*outMeanAbsChangeArrayLen = outArrayLen;
+
+	return 1;
+}
+
+EXPORT void DeleteDoubleArray(double* pointer)
+{
+	delete[] pointer;
+}
 
 
