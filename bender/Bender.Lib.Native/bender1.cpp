d19a0024ccd68cb3ba9883738e0ecf25209ae2ab
Shane DiDona <shanedidona@gmail.com>
2026-07-24 21:26:29 -0400

    [BENDER-29] 4x      if (((i - j) % 2 == 0) == evenMod)

--- bender/Bender.Lib.Native/bender1.cpp
+++ bender/Bender.Lib.Native/bender1.cpp
@@ -376,6 +376,8 @@ EXPORT int JacobiRBSingleStage(
 			{
 				int i = 0;
 				int j = 0;
+				if (((i - j) % 2 == 0) == evenMod)
+				{
 					if (TwoDRead(id, i, j, ny) == 0)
 					{
 						double neighborMean = 0.5 * (TwoDRead(v, i + 1, j, ny) + TwoDRead(v, i, j + 1, ny));
@@ -385,12 +387,15 @@ EXPORT int JacobiRBSingleStage(
 						numResid++;
 						TwoDModify(v, i, j, ny, -relaxationFactor * residual);
 					}
+				}
 			}
 
 			//i==nx-1; j==0
 			{
 				int i = nx - 1;
 				int j = 0;
+				if (((i - j) % 2 == 0) == evenMod)
+				{
 					if (TwoDRead(id, i, j, ny) == 0)
 					{
 						double neighborMean = 0.5 * (TwoDRead(v, i - 1, j, ny) + TwoDRead(v, i, j + 1, ny));
@@ -400,12 +405,15 @@ EXPORT int JacobiRBSingleStage(
 						numResid++;
 						TwoDModify(v, i, j, ny, -relaxationFactor * residual);
 					}
+				}
 			}
 
 			//i==0; j==ny-1
 			{
 				int i = 0;
 				int j = ny - 1;
+				if (((i - j) % 2 == 0) == evenMod)
+				{
 					if (TwoDRead(id, i, j, ny) == 0)
 					{
 						double neighborMean = 0.5 * (TwoDRead(v, i + 1, j, ny) + TwoDRead(v, i, j - 1, ny));
@@ -415,12 +423,15 @@ EXPORT int JacobiRBSingleStage(
 						numResid++;
 						TwoDModify(v, i, j, ny, -relaxationFactor * residual);
 					}
+				}
 			}
 
 			//i==nx-1; j==ny-1
 			{
 				int i = nx - 1;
 				int j = ny - 1;
+				if (((i - j) % 2 == 0) == evenMod)
+				{
 					if (TwoDRead(id, i, j, ny) == 0)
 					{
 						double neighborMean = 0.5 * (TwoDRead(v, i - 1, j, ny) + TwoDRead(v, i, j - 1, ny));
@@ -430,6 +441,7 @@ EXPORT int JacobiRBSingleStage(
 						numResid++;
 						TwoDModify(v, i, j, ny, -relaxationFactor * residual);
 					}
+				}
 			}
 
 		}
