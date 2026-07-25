822113ca628ef575d18ede783f578295f8f131b7
Shane DiDona <shanedidona@gmail.com>
2026-07-24 22:17:39 -0400

    [BENDER-29] change to public static (double[] MeanAbsChangeArray, bool Finished) SolveFieldCPPJacobiRB(

--- bender/Bender.Lib.NET/BenderMath.cs
+++ bender/Bender.Lib.NET/BenderMath.cs
@@ -93,14 +93,14 @@ namespace Bender.Lib.NET
             return InteropClass.Solve2DFieldSingleStageCPP(electrostaticGrid2D.V, electrostaticGrid2D.ID, relaxationFactor, meanAbsChangeStop, maxTries);
         }
 
-        public static (double[] MeanAbsChangeArray, bool Finished) SolveFieldCPP(
+        public static (double[] MeanAbsChangeArray, bool Finished) SolveFieldCPPJacobiRB(
                 ElectrostaticGrid2D electrostaticGrid2D,
                 double relaxationFactor,
                 double meanAbsChangeStop,
                 int maxTries
             )//TODO:  this will be slow and replaced with a version that does demagnified versions first
         {
-            return InteropClass.Solve2DFieldSingleStageCPP(electrostaticGrid2D.V, electrostaticGrid2D.ID, relaxationFactor, meanAbsChangeStop, maxTries);
+            return InteropClass.Solve2DFieldSingleStageCPP(electrostaticGrid2D.V, electrostaticGrid2D.ID, relaxationFactor, meanAbsChangeStop, maxTries, true);
         }
 
         public static void SolveFieldMulti(
